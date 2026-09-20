2a5e8a8fc5554f88b11bd4f5052e790d69691563
magagne <MatthieuGagne@gmail.com>
2026-09-20 14:29:44 -0400

    Use fixed build ID

--- src/vial-qmk/util/build_id.py
+++ src/vial-qmk/util/build_id.py
@@ -1,8 +1,6 @@
-import random
-
-
 def main():
-    print("#define BUILD_ID ((uint32_t)0x{:08X})".format(random.randrange(0, 2 ** 24 - 1)))
+
+    print("#define BUILD_ID ((uint32_t)0x3BA029)")
 
 
 if __name__ == "__main__":
